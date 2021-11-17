b44f1af8967eabc85185a07c01d3c4b1fd598908
chenjianhui01 <740386906@qq.com>
2021-11-17 18:39:46 +0800

    增加lombok

---
+++ src/main/java/com/cjh/domain/Student.java
@@ -0,0 +1,23 @@
+package com.cjh.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Description:
+ * @author: chenJianHui
+ * @date: 2021/11/17 18:34
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    private String name;
+    private Integer age;
+    private String sex;
+
+}
--- src/test/java/com/cjh/TestDemo.java
+++ src/test/java/com/cjh/TestDemo.java
@@ -1,5 +1,6 @@
 package com.cjh;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
  * @author: chenJianHui
  * @date: 2021/11/15 16:01
  */
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = TestDemo.class)
 public class TestDemo {
