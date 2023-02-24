7b12dd037b8c1abf0f15df9a35d621613bdd01a5
chenjianhui <chenjh1@sugon.com>
2023-02-24 13:41:10 +0800

    test 3

--- src/main/java/com/cjh/controller/HelloController.java
+++ src/main/java/com/cjh/controller/HelloController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String showHello() {
-        return "Hello world  !!!";
+        return "Hello world !";
     }
 
     @RequestMapping("/test")
